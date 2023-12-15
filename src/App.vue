f129891cb94082e2603da1d9d53b4284621a2db2
nizarsaidicoder <nizarsaidicoder@gmail.com>
2023-12-15 23:34:54 +0100

    SAE_WEB_S1_V0.1

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+  import Navbar from "@/components/Navbar.vue";
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <div class="container">
+    <h2>01 ACCUEIL</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import "./assets/css/variables";
+  .container {
+    margin-top: 9rem;
+  }
+  h2 {
+    -webkit-text-stroke: 1px white;
+    position: relative;
+    display: inline-block;
+    font-size: 96px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: 24px; /* 25% */
+    letter-spacing: 9.6px;
+    color: rgba(0, 0, 0, 0);
+    transition: all 0.6s cubic-bezier(0.075, 0.82, 0.165, 1);
+    &:after {
+      background: none repeat scroll 0 0 transparent;
+      position: absolute;
+      display: block;
+      bottom: 0;
+      content: "";
+      height: 3px;
+      left: 60%;
+      bottom: -200%;
+      background: $secondary;
+      transition: width 0.3s ease 0s, left 0.3s ease 0s, height 0.1s ease 0s;
+      width: 0;
+    }
+    &:hover:after {
+      width: 100%;
+      left: 0;
+    }
+    &:hover {
+      color: $secondary;
+      -webkit-text-stroke: 0px white;
+      padding-left: 8rem;
+    }
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,123 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://cdn-uicons.flaticon.com/2.0.0/uicons-regular-rounded/css/uicons-regular-rounded.css" />
+  <div>
+    <nav class="navbar">
+      <div class="navbar-container">
+        <img
+          src="../assets/images/Logo.svg"
+          alt="logo "
+          class="logo-img highlight highlight-full" />
+        <ul class="navbar-list">
+          <RouterLink
+            class="navbar-item"
+            to="/"
+            ><i class="fi fi-rr-home navbar-item-icon"></i>
+            <div class="navbar-item-text">Accueil</div></RouterLink
+          >
+          <RouterLink
+            class="navbar-item"
+            to="/blog"
+            ><i class="fi fi-rr-route navbar-item-icon"></i>
+            <div class="navbar-item-text">Carte Distance</div></RouterLink
+          >
+          <RouterLink
+            class="navbar-item"
+            to="/contact"
+            ><i class="fi fi-rr-ball-pile navbar-item-icon"></i>
+            <div class="navbar-item-text">Boules maximales</div></RouterLink
+          >
+          <RouterLink
+            class="navbar-item"
+            to="/about"
+            ><i class="fi fi-rr-build-alt navbar-item-icon"></i>
+            <div class="navbar-item-text">Reconstruction</div></RouterLink
+          >
+          <RouterLink
+            class="navbar-item"
+            to="/about"
+            ><i class="fi fi-rr-images navbar-item-icon"></i>
+            <div class="navbar-item-text">A propos</div></RouterLink
+          >
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    setup() {
+      return {};
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "@/assets/css/variables";
+  .navbar {
+    background-color: $primary;
+    &-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin: 0 auto;
+      padding: 1.6rem 2rem;
+    }
+    &-item {
+      color: $accent;
+      text-transform: uppercase;
+      text-decoration: none;
+      font-weight: 600;
+      letter-spacing: 0.1rem;
+      font-size: 1.6rem;
+      padding: 0.8rem 0;
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 1rem;
+      &:hover {
+        .navbar-item-icon {
+          font-size: 3.2rem;
+          transition: all cubic-bezier(0.39, 0.575, 0.565, 1) 0.3s;
+        }
+      }
+      &:after {
+        background: none repeat scroll 0 0 transparent;
+        position: absolute;
+        display: block;
+        bottom: 0;
+        content: "";
+        height: 3px;
+        left: 50%;
+        border-radius: 3px;
+        background: $secondary;
+        transition: width 0.3s ease 0s, left 0.3s ease 0s, height 0.1s ease 0s;
+        width: 0;
+      }
+      &:hover:after {
+        width: 100%;
+        left: 0;
+      }
+      &:active:after {
+        height: 5px;
+        width: 100%;
+      }
+    }
+  }
+  /* LOGO */
+  .logo-img {
+    height: 6.4rem;
+  }
+
+  /* NAVBAR LIST */
+  ul {
+    padding: 0;
+    list-style: none;
+    display: flex;
+    gap: 6rem;
+    font-size: 1.8rem;
+  }
+</style>
